b4dbec8442b5ec6050f0a20915fe51462316fd9b
BrayanPacheco <81273596+BrayanPacheco@users.noreply.github.com>
2021-03-24 00:31:12 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/* Initializa Firebase con la configuracion del proyecto. Revisa la 
+ * configuración en tu servidor de Firebase */
+// @ts-ignore
+firebase.initialize.App(){
+    apiKey: "AIzaSyDxEBeRh15_JsgWyzPrHrHwXvy3H4FZBS4",
+    authDomain: "autentica42bpl.firebaseapp.com",
+    projectId: "autentica42bpl",
+    storageBucket: "autentica42bpl.appspot.com",
+    messagingSenderId: "73188714008",
+    appId: "1:73188714008:web:ae81db4263152bd192056d",
+    measurementId: "G-20MSS3JQ0D"
+});
